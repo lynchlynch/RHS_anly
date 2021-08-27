fc51192c01cc7a3398b5c02f69bccdf5dcb9450e
lynchlynch <yepei1988@163.com>
2021-08-27 16:59:33 +0800

    12

--- rhs_anly.py
+++ rhs_anly.py
@@ -11,13 +11,13 @@ import chi_test as ct
 
 raw_path = '/Users/pei/pydir/RHS_anly/raw_data/'
 result_path = '/Users/pei/pydir/RHS_anly/result/'
-rhs_log = pd.read_csv(raw_path + 'RHS Usage Log-Summary-update 20210827.csv')
+# rhs_log = pd.read_csv(raw_path + 'RHS Usage Log-Summary-update 20210827.csv')
+rhs_log = pd.read_excel(raw_path + 'RHS Usage Log-Summary-update 20210827.xlsx',engine='openpyxl',sheet_name='Log')
+# print(rhs_log)
 # print(rhs_log['Request Year-Month'].tolist())
 max_cab_pwr = pd.read_csv(raw_path + 'max_cab_power.csv')
 site_list = max_cab_pwr['Site ID'].tolist()
 total_device_list = pd.read_csv(raw_path + 'network device list.csv')
-# print(total_device_list[total_device_list['Site ID'] == site_list[0]])
-# print(site_list)
 
 pwr_file_list = os.listdir(raw_path + 'yearly_power_data/')
 for single_file in pwr_file_list:
@@ -48,20 +48,6 @@ for single_site in tqdm(site_list,desc='pwr-rhs'):
         # plt.legend(loc=2)
         plt.savefig(result_path + single_site + '_power-RHS.png', bbox_inches='tight')
         plt.close()
-'''
-bjs_power_avg = pd.read_csv(raw_path + 'yearly_power_data/CU Beijing.csv')
-bjs_power_avg = pi.power_integrated(bjs_power_avg)
-bjs_power_avg = dz.de_zero(bjs_power_avg)
-plt.plot(bjs_power_avg['Value'].tolist())
-# plt.twinx()
-date_list_sample,rhs_times_index_list,rhs_times_list = rt.rhs_times(rhs_log,'CU Beijing',bjs_power_avg)
-plt.xticks(rhs_times_index_list,date_list_sample,color='blue',rotation=60)
-plt.twinx()
-plt.plot(rhs_times_index_list,rhs_times_list,'ro-')
-plt.title('bjs_cu_pwr')
-plt.savefig(result_path + 'bjs_pwr.png',bbox_inches='tight')
-plt.close()
-'''
 
 bjs_power_avg = pd.read_csv(raw_path + 'yearly_power_data/CU Beijing.csv')
 bjs_power_avg = pi.power_integrated(bjs_power_avg)
@@ -130,7 +116,7 @@ for annote_index in range(len(rhs_times_index_list)):
     plt.annotate(max_site_list[annote_index], xy=(rhs_times_index_list[annote_index], max_fault_rate_list[annote_index]),
                  xytext=(rhs_times_index_list[annote_index], max_fault_rate_list[annote_index] + (-1)**(annote_index%2) * 0.5),
                  xycoords='data',arrowprops=dict(facecolor='blue', shrink=0.05))
-plt.title('highest_fault_rate.png')
+plt.title('highest_fault_rate')
 # plt.show()
 plt.savefig(result_path + 'highest_fault_rate.png',bbox_inches='tight')
 plt.close()
@@ -156,18 +142,47 @@ plt.title('consumale/ticket')
 plt.xticks(rhs_times_index_list,date_list_sample,color='blue',rotation=60)
 plt.savefig(result_path + 'ticket-consumale.png',bbox_inches='tight')
 plt.close()
-'''
-date_list_sample, rhs_times_index_list, total_module_num_list = rc.rhs_consumable(rhs_log,'CU Beijing',bjs_power_avg)
-# plt.plot(rhs_times_index_list,rhs_times_list,'ro-')
-# plt.plot(rhs_times_index_list, total_module_num_list,'b^-')
-csmb_tkt = []
-for index in range(len(rhs_times_list)):
-    if rhs_times_list[index] == 0:
-        csmb_tkt.append(0)
+
+current_year = int((rhs_log['Request Year-Month'].tolist()[-1]).split('-')[0])
+current_month = int((rhs_log['Request Year-Month'].tolist()[-1]).split('-')[1])
+process_period = 6
+current_year_month_list = []
+for index in range(process_period):
+    if (current_month - index) < 1:
+        current_year_month = str(current_year-1) + '-' + str(12-current_month+1)
     else:
-        csmb_tkt.append(round(total_module_num_list[index]/rhs_times_list[index],3))
-plt.plot(rhs_times_index_list, csmb_tkt,'b^-')
-plt.title('bjs_cu consumale/ticket')
-plt.savefig(result_path + 'bjs_cu ticket-consumale.png',bbox_inches='tight')
+        current_year_month = str(current_year) + '-' + str(current_month-index)
+        # print(current_year_month)
+    current_year_month_list.append(current_year_month)
+
+rhs_times_list = []
+token_qty_list = []
+for index in range(process_period):
+    current_year_month = current_year_month_list[process_period-index-1]
+    rhs_times = len(rhs_log[rhs_log['Request Year-Month']==current_year_month])
+    rhs_times_list.append(rhs_times)
+    token_num_per_month = sum(rhs_log[rhs_log['Request Year-Month']==current_year_month]['Number of Tokens'].tolist())
+    token_qty_list.append(token_num_per_month)
+
+# print(rhs_times_list)
+# print(token_qty_list)
+
 plt.close()
-'''
+plt.figure()
+plt.style.use('dark_background')
+plt.plot(list(range(process_period)),rhs_times_list,'o-',color='orange',label='Ticket')
+plt.plot(list(range(process_period)),token_qty_list,'v-',color='cyan',label='Token')
+# print(current_year_month_list)
+current_year_month_list_reverse = []
+for index in range(process_period):
+    current_year_month_list_reverse.append(current_year_month_list[process_period-index-1])
+    plt.text(index,rhs_times_list[index]-0.6,rhs_times_list[index])
+    plt.text(index,token_qty_list[index]+0.2, token_qty_list[index])
+# print(current_year_month_list_reverse)
+# print(list(range(process_period)))
+x_index = list(range(process_period))
+plt.xticks(x_index,current_year_month_list_reverse,rotation=45)
+plt.legend()
+
+plt.title('Ticket-Tocken')
+plt.savefig(result_path + 'ticket_token')
--- test2.py
+++ test2.py
@@ -1,8 +1,4 @@
-import numpy as np
-a = np.array([4,2,3])
-b = np.array([2,5,7])
-
-c = a/b
-d = np.multiply(a,b)
-e = (a-b) ** 2/ b
-print(e)
+a = list(range(6))
+print(a)
+a.reverse()
+print(a)
